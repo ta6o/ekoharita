90f85988b143ff8e045d571c8cf0057bee26099b
yakup cetinkaya <yakup.cetinkaya@gmail.com>
2017-05-04 16:00:03 +0300

    hop

--- cc.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-[[ -s "/usr/local/rvm/scripts/rvm" ]] && . "/usr/local/rvm/scripts/rvm"
-source /usr/local/rvm/scripts/rvm
-rvm use 2.1.2@tesev
-
-cd /var/www/cc
-/usr/local/rvm/rubies/ruby-2.1.2/bin/ruby ./update.rb fetch
-
